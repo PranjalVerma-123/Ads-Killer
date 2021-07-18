ef5aad2b093a558845127c3c61a28a9e525ed446
PranjalVerma-123 <61972103+PranjalVerma-123@users.noreply.github.com>
2021-07-18 20:16:34 +0530

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+  
+  var skipbtn = document.getElementsByClassName("ytp-ad-skip-button");
+  if(skipbtn != undefined && skipbtn.length >0){
+    console.log("Advirtesement detected");
+    skipbtn[0].click()
+  }
+},300)
